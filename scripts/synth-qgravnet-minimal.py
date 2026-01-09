1466860ce5958281eddb7c69a6889242ac992e63
morunner <107193613+morunner@users.noreply.github.com>
2026-01-09 13:39:02 +0100

    move extensions registering away

--- scripts/synth-qgravnet-minimal.py
+++ scripts/synth-qgravnet-minimal.py
@@ -4,24 +4,11 @@ import argparse
 import os
 
 import hls4ml
-from hls4ml_gravnet.hls4ml_extension.global_exchange import HGlobalExchange
-from hls4ml_gravnet.hls4ml_extension.global_exchange_parser import parse_global_exchange
-from hls4ml_gravnet.hls4ml_extension.global_exchange_template import (
-    GlobalExchangeConfigTemplate,
-    GlobalExchangeFunctionTemplate,
-)
-from hls4ml_gravnet.hls4ml_extension.gravnet_core import HGravNetCore
-from hls4ml_gravnet.hls4ml_extension.gravnet_core_parser import parse_gravnet_layer
-from hls4ml_gravnet.hls4ml_extension.gravnet_core_template import GravNetCoreConfigTemplate, GravNetCoreFunctionTemplate
-from hls4ml_gravnet.keras_models.qgravnet_minimal import QGravNetMinimalFactory
-from utils.config import set_qgravnet_hls_config
+from hls4ml_gravnet.keras.qgravnet_minimal import QGravNetMinimalFactory
+from utils.config import hls4ml_gravnet_register_extensions, set_qgravnet_hls_config
 from utils.data import load_processed
 from utils.evaluation import load_run, response_rmse
-from utils.files import HLS4ML_OUT_PATH, PROJECT_ROOT, RESULTS_PATH
-
-N_VERTICES = 128
-N_FEATURES = 4
-N_SAMPLES = 10
+from utils.files import HLS4ML_OUT_PATH, RESULTS_PATH
 
 
 def parse_args() -> argparse.Namespace:
@@ -30,6 +17,7 @@ def parse_args() -> argparse.Namespace:
     )
     parser.add_argument('-vhls', '--vitis_hls_path')
     parser.add_argument('-n', '--project_name')
+    parser.add_argument('-r', '--reuse', type=int)
 
     return parser.parse_args()
 
@@ -38,8 +26,7 @@ def main():
     args = parse_args()
     os.environ['PATH'] = args.vitis_hls_path + 'bin:' + os.environ['PATH']
 
-    model_cfg, _, _, datapath = load_run(RESULTS_PATH / 'train_new_quantization_cfg')
-
+    model_cfg, _, _, datapath = load_run(RESULTS_PATH / 'nn_8')
     D = load_processed(datapath)
 
     keras_model = QGravNetMinimalFactory(n_neighbours=model_cfg['n_neighbours']).create_keras_model(
@@ -54,21 +41,17 @@ def main():
             'classification': D['y_pid_train'],
         },
     )
+    print(keras_model.summary())
 
-    hls4ml.converters.register_keras_v2_layer_handler('GravNetCore', parse_gravnet_layer)
-    hls4ml.converters.register_keras_v2_layer_handler('GlobalExchange', parse_global_exchange)
-    hls4ml.model.layers.register_layer('GravNetCore', HGravNetCore)
-    hls4ml.model.layers.register_layer('GlobalExchange', HGlobalExchange)
-    backend = hls4ml.backends.get_backend('Vitis')
-    backend.register_template(GravNetCoreConfigTemplate)
-    backend.register_template(GravNetCoreFunctionTemplate)
-    backend.register_template(GlobalExchangeConfigTemplate)
-    backend.register_template(GlobalExchangeFunctionTemplate)
-    backend.register_source(PROJECT_ROOT / 'hls4ml_gravnet' / 'hls' / 'nnet_gravnet_core.h')
-    backend.register_source(PROJECT_ROOT / 'hls4ml_gravnet' / 'hls' / 'nnet_global_exchange.h')
+    hls4ml_gravnet_register_extensions()
 
-    hls_config = hls4ml.utils.config_from_keras_model(model=keras_model, granularity='name', backend='Vitis')
+    hls_config = hls4ml.utils.config_from_keras_model(
+        model=keras_model, granularity='name', backend='Vitis', default_reuse_factor=32
+    )
     set_qgravnet_hls_config(hls_config)
+    hls_config['LayerName']['q_dense']['ReuseFactor'] = args.reuse
+    hls_config['Model']['PipelineStyle'] = 'dataflow'
+
     hls_model = hls4ml.converters.convert_from_keras_model(
         model=keras_model,
         hls_config=hls_config,
@@ -77,12 +60,7 @@ def main():
         backend='Vitis',
     )
     hls_model.compile()
-    hls_model.build(
-        csim=True,
-        synth=True,
-        cosim=True,
-        validation=True,
-    )
+    hls_model.build(csim=True, synth=True, cosim=True, validation=True, vsynth=True)
 
 
 if __name__ == '__main__':
